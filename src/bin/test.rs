ed21b0c6c367df4701d802322e0815aca91afbd9
paddymills <paddymills@pm.me>
2023-03-16 15:22:30 -0400

    add xl parsing

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -1,9 +1,21 @@
+use std::path::PathBuf;
 
-// main for testing
+use calamine::{Reader, open_workbook, Xlsx, DataType};
+
 fn main() -> std::io::Result<()> {
-    let app = sap_error_utils::apps::SapInboxApp::default();
-    
-    app.generate_comparison()?;
+
+    let userprofile = match std::env::var_os("USERPROFILE") {
+        Some(path) => path,
+        None => panic!("Could not locate env variable `USERPROFILE`")
+    };
+
+    let path = PathBuf::from(format!("{}/Documents/SAP/SAP GUI/export.xlsx", userprofile.to_str().unwrap()));
+    let mut wb: Xlsx<_> = open_workbook(path).expect("Cannot open file");
+    if let Some(Ok(rng)) = wb.worksheet_range("Sheet1") {
+        for row in rng.rows() {
+            println!("{:?}", row);
+        }
+    }
 
     Ok(())
 }
