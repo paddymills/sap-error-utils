d56774344427649d6994110cc5bef6f4c1a1248d
Patrick Miller <paddymills@proton.me>
2023-12-27 14:01:57 -0500

    stupid powershell syntax

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,6 @@
 cargo build --release --all-features
 Copy-Item .\target\release\inbox_errors.exe '\\hssfileserv1\shops\inventory\sap'
 
-if ($env:USERNAME == "PMiller1") {
+if ($env:USERNAME = "PMiller1") {
     Copy-Item .\target\release\inbox_errors.exe "$env:USERPROFILE\src\cogi\inbox"
 }
