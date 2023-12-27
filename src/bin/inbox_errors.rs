84d19721e9693a15ddb5740c1a4727bc2caff3c4
Patrick Miller <paddymills@proton.me>
2023-12-27 13:51:16 -0500

    hide terminal for non-debug

--- src/bin/inbox_errors.rs
+++ src/bin/inbox_errors.rs
@@ -1,5 +1,6 @@
 
-#![windows_subsystem = "windows"]
+// hide terminal window, if not a debug build and terminal feature is not enabled
+#![cfg_attr(all(not(debug_assertions), not(feature = "terminal")), windows_subsystem = "windows")]
 
 // TODO: cli
 fn main() -> eframe::Result<()> {
